c262e9aa3db64939dcac9fe4c03507a0f0158698
Julien Gunther <jgunther021@gmail.com>
2021-05-07 07:05:01 +0200

    loop instead of crash handled by pm2

--- main.py
+++ main.py
@@ -44,33 +44,31 @@ if __name__ == '__main__':
         
         # Create IMAP_SSL instance
         M = IMAP4_SSL(IMAP_SERVER)
-        M.login(EMAIL, PASSWORD)
-        
-        # needed to get rid of the Auth state
-        M.select("INBOX")
-        
-        # start Idler
-        idler = Idler(M)
-        idler.start()
 
         # every 28 min and 59 seconds, reboot to prevent the bot to get out of his idle state
         while True:
+            M.login(EMAIL, PASSWORD)
+        
+            # needed to get rid of the Auth state
+            M.select("INBOX")
+            
+            # start Idler
+            idler = Idler(M)
+            idler.start()
             print(f"[{datetime.now(timezone(TIMEZONE)).strftime('%d/%m/%Y %H:%M:%S')}] : App online !")
             sleep(1739) # IMAP4_SSL.Idle_timout - 1s
-            raise Exception
+            print("Closing threads and logging out...")
+            # close threads
+            idler.stop()
+            idler.join()
+
+            # close IMAP instance and logout (DON'T FORGET TO LOGOUT)
+            M.close()
+            M.logout()
+            print("Disconnected !")
+            # log when the bot disconnect (I use an external logger that catch print())
+            print(f"[{datetime.now(timezone(TIMEZONE)).strftime('%d/%m/%Y %H:%M:%S')}] : App offline")
 
     except Exception as e:
-        # print(e)
-        print("Closing threads and logging out...")
-        # close threads
-        idler.stop()
-        idler.join()
-
-        # close IMAP instance and logout (DON'T FORGET TO LOGOUT)
-        M.close()
-        M.logout()
-        print("Disconnected !")
-        # log when the bot disconnect (I use an external logger that catch print())
-        print(f"[{datetime.now(timezone(TIMEZONE)).strftime('%d/%m/%Y %H:%M:%S')}] : App offline")
-        # handled by pm2, automatically restart main.py when it exits
-        exit()
+        print(e)
+        pass
